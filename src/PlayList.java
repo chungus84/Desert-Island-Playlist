8a536b08af3a5cf3c7dabe55e8b63f71443ec1a6
kenichi beveridge <kenbev84@gmail.com>
2023-03-22 12:25:35 +0000

    add a number of songs to desertIslandPlaylist ArrayList"

--- src/PlayList.java
+++ src/PlayList.java
@@ -4,6 +4,12 @@ public class PlayList {
     // class to create a dessert island play list
     public static void main(String[] args) {
         ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
-
+        desertIslandPlaylist.add("Cochise");
+        desertIslandPlaylist.add("Jeremy");
+        desertIslandPlaylist.add("Freedom");
+        desertIslandPlaylist.add("Soulbound");
+        desertIslandPlaylist.add("Greatest Show on Earth");
+        desertIslandPlaylist.add("Holy War");
+        desertIslandPlaylist.add("Rainbow in the Dark");
     }
 }
