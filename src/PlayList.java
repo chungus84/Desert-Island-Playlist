b39995c336faddd88ca3fda7a22edd92a3f3ebd1
kenichi beveridge <kenbev84@gmail.com>
2023-03-22 15:35:40 +0000

    Add updatePlaylist() method and doc strings and comments

--- src/PlayList.java
+++ src/PlayList.java
@@ -4,15 +4,18 @@ public class PlayList {
     // class to create a dessert island play list
     ArrayList<String> playlist = new ArrayList<String>();
 
+    // Constructor
     public PlayList() {
 
     }
 
     public ArrayList getPlayList() {
+        /** returns all items in playlist */
         return playlist;
     }
 
     public void addToPlaylist(String[] list) {
+        /** adds list of songs to playlist */
         for (String song : list) {
             playlist.add(song);
             System.out.println(song + " was added to playlist");
@@ -20,47 +23,37 @@ public class PlayList {
     }
 
     public int getPlaylistSize() {
+        /** retyrns size of playlist */
         return playlist.size();
     }
 
     public void removeFromPlaylist(String songToRemove) {
+        /** removes song from playlist if it is currently a value in the playlist*/
         if (playlist.contains(songToRemove)) {
-            System.out.println(songToRemove + " has been removed from playlist");
             playlist.remove(songToRemove);
+            System.out.println(songToRemove + " has been removed from playlist");
         } else {
             System.out.println(songToRemove + " is not in this playlist");
+        }
+    }
+
+    public void updatePlaylist(String songToUpdate, String replacementSong) {
+        /** updates song by searching for the index of a song then sets it to a new value */
+        if (playlist.contains(songToUpdate)) {
+            playlist.set(playlist.indexOf(songToUpdate), replacementSong);
+            System.out.println(songToUpdate + " has been replaced by " + replacementSong + " in the playlist");
+        } else {
+            System.out.println( songToUpdate + " does not exist in this playlist");
         }
     }
 
 
 
     public static void main(String[] args) {
-//        ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
-//        desertIslandPlaylist.add("Cochise");
-//        desertIslandPlaylist.add("Jeremy");
-//        desertIslandPlaylist.add("Freedom");
-//        desertIslandPlaylist.add("Soulbound");
-//        desertIslandPlaylist.add("Greatest Show on Earth");
-//        desertIslandPlaylist.add("Holy War");
-//        desertIslandPlaylist.add("Rainbow in the Dark");
 //
-//        System.out.println(desertIslandPlaylist);
-//        System.out.println(desertIslandPlaylist.size());
-//
-//        desertIslandPlaylist.remove(5);
-//        System.out.println(desertIslandPlaylist.size());
-//        desertIslandPlaylist.remove("Jeremy");
-//        System.out.println(desertIslandPlaylist.size());
-//        System.out.println(desertIslandPlaylist);
-//
-//        int a = desertIslandPlaylist.indexOf("Cochise");
-//        int b = desertIslandPlaylist.indexOf("Rainbow in the Dark");
-//
-//        desertIslandPlaylist.set(a, "Jeremy");
-//        desertIslandPlaylist.set(b, "Holy War");
-//        System.out.println(desertIslandPlaylist);
 
 
+        // Testing functionality
         String[] songsList = { "Cochise", "Jeremy", "Freedom", "Soulbound", "Greatest Show on Earth", "Holy War", "Rainbow in the Dark"};
 
         PlayList desertIslandPlaylist = new PlayList();
@@ -73,6 +66,9 @@ public class PlayList {
         System.out.println(desertIslandPlaylist.getPlaylistSize());
         desertIslandPlaylist.removeFromPlaylist("Song 2");
         desertIslandPlaylist.removeFromPlaylist("Jeremy");
+        desertIslandPlaylist.updatePlaylist("Soulbound", "Dice");
+        desertIslandPlaylist.updatePlaylist("Song 2", "Evenflow");
+        System.out.println(desertIslandPlaylist.getPlayList());
 
     }
 }
